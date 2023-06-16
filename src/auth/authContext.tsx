da094ec959e8023ecdb1e53beb0d6d5740c259e2
TaniaMitnovych <taniamitnovych15@gmail.com>
2023-06-16 12:45:26 +0300

    add sso with google, create SignInPage

--- src/auth/authContext.tsx
+++ src/auth/authContext.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 export const UserContext = React.createContext(null);
+<<<<<<< HEAD
+=======
+export const UserDispatchContext = React.createContext(null);
+
+>>>>>>> c015a2d (add sso with google, create SignInPage)
 
 const AuthContext: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [user, setUser] = React.useState(null);
 
   return (
+<<<<<<< HEAD
     <UserContext.Provider value={{ user, setUser }} >
       {children}
+=======
+    <UserContext.Provider value={user} >
+      <UserDispatchContext.Provider value={setUser}>
+        {children}
+      </UserDispatchContext.Provider>
+>>>>>>> c015a2d (add sso with google, create SignInPage)
     </UserContext.Provider>
   )
 }
