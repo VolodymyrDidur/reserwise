8a697dba893bc7943260ff69452ca8c658b4dc7b
Yurii <yurakupchunsky@gmail.com>
2023-06-25 19:49:58 +0300

    minor changes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,15 @@
 import {Route, Switch} from 'react-router-dom';
 import {MainPage} from '../pages';
+import { AccountPage } from '../pages/AccountPage/AccountPage';
+import { SignInPage } from '../pages/SignInPage/SignInPage';
+
 
 const Routes = () => {
     return (
         <Switch>
-            <Route path="/" render={() => <MainPage>Hello</MainPage>}/>
+            <Route exact path="/" render={() => <MainPage>Hello</MainPage>}/>
+            <Route path="/accountPage" render={() => <AccountPage/>}/>
+            <Route path="/signInPage" render={() => <SignInPage/>}/>
         </Switch>
     );
 };
