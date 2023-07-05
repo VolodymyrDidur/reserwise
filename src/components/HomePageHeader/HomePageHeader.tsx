3ec8beb5b7090a5b44c2e987f26386e6bdc4fa31
TaniaMitnovych <taniamitnovych15@gmail.com>
2023-07-05 19:59:19 +0300

    improved app routing

--- src/components/HomePageHeader/HomePageHeader.tsx
+++ src/components/HomePageHeader/HomePageHeader.tsx
@@ -4,18 +4,29 @@ import useUser from '../../hooks/useUser';
 import { useAuth } from '../../hooks/useAuth';
 import defaultUserImage from '../../assets/images/user-icon.png'
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 
-export const HomePageHeader: React.FC<{}> = ({}) => {
+export const HomePageHeader: React.FC<{}> = ({ }) => {
     const { t } = useTranslation();
     const { isAuthenticated } = useAuth();
     const { user } = useUser();
+    const history = useHistory();
 
     const greeting = isAuthenticated ? `${t('welcome')}, ${user.given_name} 👋` : `${t('welcomeGuest')} 👋`;
-    const UserImage = isAuthenticated ? user.picture : defaultUserImage ;
-    
+    const UserImage = isAuthenticated ? user.picture : defaultUserImage;
+
+    const onUserClickHandler = (event: React.MouseEvent<HTMLElement>):void => {
+        event.preventDefault();
+        if (isAuthenticated) {
+            history.push('/account');
+        } else {
+            history.push('/sign-in');
+        }
+    }
+
     return (
         <header className="header">
-            <div className='header__user'>
+            <div className='header__user' onClick={onUserClickHandler}>
                 <div className="header__icon">
                     <img src={UserImage} alt="User Icon" />
                 </div>
@@ -25,6 +36,6 @@ export const HomePageHeader: React.FC<{}> = ({}) => {
                 <input type="text" className="search_input-field" placeholder={t('search')} />
             </div>
         </header>
-        
+
     );
 };
