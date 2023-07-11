49f2ffde2728bfaff9edaaaca1af3922d01caca7
Tetiana Mitnovych <72037173+TaniaMitnovych@users.noreply.github.com>
2023-07-11 21:11:02 +0300

    tech/internationalization
    add internationalization of place info and favorites pages

--- src/components/FavoritesTable/FavoritesTable.tsx
+++ src/components/FavoritesTable/FavoritesTable.tsx
@@ -4,22 +4,24 @@ import PlaceCard, { FoodEstablishmentDataProps } from "../PlaceCard/PlaceCard";
 import useFavorites from '../../hooks/useFavorites';
 import FavoritesImage from '../../assets/images/favoritePageImage.png';
 import "./FavoritesTable.scss"
+import { useTranslation } from "react-i18next";
 
 const FavouritesTable: React.FC = () => {
   const favourites = useFavorites();
   const history = useHistory();
+  const { t } = useTranslation();
 
   if (favourites.length === 0) 
     return (
       <div className="empty__table">
           <img src={FavoritesImage} alt="Favorite Page Image" className="empty__table__image" />
           <div className="empty__table__title">
-            <p>You don't have any favorite restaurants yet</p>
+            <p>{t('noFavorites')}</p>
           </div>
           <div className="empty__table__description">
-            <p>For quick access to your favorite restaurants, press favorite on the restaurant you liked</p>
+            <p>{t('hintFavorites')}</p>
           </div>
-          <button className="empty__table__button" onClick={() => history.push('/home')}>Look over</button>
+          <button className="empty__table__button" onClick={() => history.push('/home')}>{t('lookOver')}</button>
       </div>
     );
     
--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './FavoritesPage.scss';
 import FavouritesTable from '../../components/FavoritesTable/FavoritesTable';
 import { PageHeader } from '../../components/PageHeader';
+import { useTranslation } from 'react-i18next';
 
 export const FavoritesPage: React.FC<{}> = ({ }) => {
+    const { t } = useTranslation();
     return (
       <div className='favorites__page'>
-        <PageHeader headerText='Favorites' />
+        <PageHeader headerText={t('favorites')} />
         <FavouritesTable />
       </div>
     );
